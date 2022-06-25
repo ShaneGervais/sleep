99e1f171b8f0fd995814b16ca3dddb29fb300803
Shane <95928338+ShaneGervais@users.noreply.github.com>
2022-06-24 22:37:50 -0300

    Sleep function fix
    Fixed sleep function by adding the <Windows.h>/<unistd.h>. This program is ran on linux (ubuntu)

--- execute.cpp
+++ execute.cpp
@@ -1,4 +1,17 @@
-//Execution function
+/**
+ * Execution function
+ * 
+ * Here we try to be OS variable.
+ * Check if were in windows or linux.
+ * We need the <Windows.h>/<unistd.h>
+ * for the sleep function at line 27.
+ * 
+ * This program was written on linux (ubuntu).
+ * 
+ * When we have a control for its number of 
+ * iterations we sleep for the ith seconds in 
+ * control and output the results.
+ */
 
 #ifdef _WIN32
 #include <Windows.h>
@@ -13,18 +26,22 @@
 
 void execute(Control *control)
 {
+    //If we have a control
     if(control)
     {
         for(int i = 0; i < control->iterations; i++)
         {
+            //Sleep for ith seconds in control
             sleep(control->seconds);
 
+            //print
             std::cout << i
                       << " sleeping for "
                       << control->seconds
                       << " seconds "
                       << std::endl;
         }
-    }
 
-}
+    }//end if
+
+}//end execute
